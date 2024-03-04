5324c563ee58de2c8ae1b0b55379e4b109a037ec
R Pradyumna Prahas <161730497+PradyumnaPrahas2@users.noreply.github.com>
2024-03-05 00:36:09 +0530

    Update gf.py

--- gf.py
+++ gf.py
@@ -4,14 +4,14 @@ import sys
 from twilio.rest import Client
 body1=str(sys.argv[1])
 phno='+91'+str(sys.argv[2])
-account_sid = 'ACc67cebefa6722ececfa5e026588fa7d5'
-auth_token = 'b33297f232ddb8d73d4e77abdad0d7fd'
+account_sid = 'enter your sid'
+auth_token = 'enter_ur_id'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
          body=body1,
-         from_='+13344542087',
+         from_='+-----------',
          to=phno,
      )
 
